7907348edd6bb4662ee6db2f6443102ebbc6783c
thecodeupboy <chirag.codeup@gmail.com>
2025-01-31 14:58:01 +0530

    reverted all changes

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const connectDB = require('./config/db');
 const adminRoutes = require('./routes/adminRoutes');
-const contentRoutes = require('./routes/contentRoutes.js');
-const path = require('path');
-require('dotenv').config();
-
+const contentRoutes = require('./routes/contentRoutes.js')
 connectDB();
+const path = require('path');
 
 const app = express();
 app.use(express.json());
-
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
@@ -25,7 +19,12 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-app.use("/admin", adminRoutes);
-app.use("/", contentRoutes);
 
-module.exports = app;
+app.use("/admin",adminRoutes);
+app.use("/",contentRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
+
+     
